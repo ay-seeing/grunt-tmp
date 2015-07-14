3e8295503475872e4957a5ea5e6bb008dbae0e69
ay-seeing <630999015@qq.com>
2015-07-14 12:53:28 +0800

    grunt template项目添加

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt){
+	// Project configuration
+	grunt.initConfig({
+		// Remove built directory
+	    clean: {
+	      build: ['asset/']
+	    },
+	    includes: {
+		  files: {
+		    src: 'origin/html/*', // Source files 
+		    dest: 'asset', // Destination directory 
+		    flatten: true,
+		    cwd: '.',
+		    options: {
+		      silent: true,
+		      banner: '<!-- I am a banner <% includes.files.dest %> -->'
+		    }
+		  }
+		}
+		// includes: {
+	 //      build: {
+	 //        cwd: 'site',
+	 //        src: 'origin/html/*',
+	 //        dest: 'asset/',
+	 //        options: {
+	 //          flatten: true,
+	 //          includePath: 'include',
+	 //          banner: '<!-- Site built using grunt includes! -->\n'
+	 //        }
+	 //      }
+	 //    }
+	});
+
+	// Load local tasks.
+    // grunt.loadTasks('tasks');
+
+	// 加载插件
+	grunt.loadNpmTasks('grunt-includes');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+
+	// 默认被执行的任务列表
+	grunt.registerTask('default', ['clean', 'includes']);
+}
+
